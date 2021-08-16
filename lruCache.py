9c8743fbf6e74d82cc95881835aba394a9858f5f
Samuelsuhas96 <bellapusamuelsuhas@msitprogram.net>
2021-08-16 09:51:54 +0530

    Added constructor

---
+++ lruCache.py
@@ -0,0 +1,8 @@
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+    
+cache = LRUCache(2)
